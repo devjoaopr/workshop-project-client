5fbb7277091e35efc9b0385ffc644e10a6466186
devjoaopr <joaoprsantos1@outlook.com>
2026-05-17 20:34:35 -0300

    testing finance

--- workshop-client/src/app/app.routes.ts
+++ workshop-client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Finance } from './features/finance/finance';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'finance',
+    component: Finance,
+  },
+];
--- workshop-client/src/app/features/finance/finance.ts
+++ workshop-client/src/app/features/finance/finance.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FinanceDTO } from './finance-dto/finance-dto';
+import { Finance as FinanceService } from './finance-service/finance';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
 @Component({
+  standalone: true,
   selector: 'app-finance',
   imports: [],
   templateUrl: './finance.html',
   styleUrl: './finance.css',
 })
-export class Finance {}
+export class Finance implements OnInit {
+  finance: FinanceDTO[] | null = null;
+
+  private fnService = inject(FinanceService);
+
+  ngOnInit(): void {
+    this.loadFinance();
+  }
+  loadFinance() {
+    this.fnService.listAll().subscribe((data) => {
+      this.finance = data;
+    });
+  }
+}
