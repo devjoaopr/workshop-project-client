30ea877935d6da37879312bd6a51315f5439eb06
devjoaopr <joaoprsantos1@outlook.com>
2026-05-24 18:24:17 -0300

    login page done, now adding the homepage

---
+++ workshop-client/src/app/features/homepage/homepage.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.css',
+})
+export class Homepage implements OnInit {
+  ngOnInit(): void {}
+}
